8012ab0f264b97f353fa337d19e0fdb2f37e27b4
dialloflatno <brahimdiallo23@gmail.com>
2021-10-05 12:43:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+undefined
+dodger.style.backgroundColor = "#000000";
+
+'#000000'
+dodger.style.backgroundColor = "#FF69B4";
+
+'#FF69B4'
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 1}px`;
+  }
+});
+undefined
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+//RIGHT//
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight") {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 1}px`;
+  }
+});
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
